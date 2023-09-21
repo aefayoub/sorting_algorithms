31db4f9b65f77d48914ef9543d5c3fb011ecd944
aefayoub <aef.ayoub@gmail.com>
2023-09-21 08:46:45 -0700

    cmt task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,7 +21,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				tmp = array[i+ 1];
+				tmp = array[i + 1];
 				array[i + 1] = array[i];
 				array[i] = tmp;
 				print_array(array, size);
